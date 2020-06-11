49236002ab0b1c61ed5d86cc8163b91c3193dff2
LEv145 <40071054+LEv145@users.noreply.github.com>
2020-06-11 12:04:56 +0200

    Update speaker.py

--- main/speaker.py
+++ main/speaker.py
@@ -2,5 +2,5 @@ import main
 
 my_text = "Test text"
 my_lang = "en"
-speaker = Text_to_speak(my_text, my_lang) # Создаём класс
+speaker = Text_to_speak(my_text, my_lang) # Создаём объект класса
 speaker.to_mp3() # Создем mp3 файл с озвученным текстом
